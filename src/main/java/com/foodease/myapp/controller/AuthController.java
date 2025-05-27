ca0da2df2dccc718dba3a24b1ca035f6cf8b07eb
HongQuan <qp57744@gmail.com>
2025-05-28 01:14:16 +0700

    quan: allow cross auth

--- src/main/java/com/foodease/myapp/controller/AuthController.java
+++ src/main/java/com/foodease/myapp/controller/AuthController.java
@@ -13,10 +13,7 @@ import com.nimbusds.jose.JOSEException;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.apache.coyote.BadRequestException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
@@ -24,6 +21,7 @@ import java.text.ParseException;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     AuthService authService;
     UserService userService;
